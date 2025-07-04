5425659dcd9f17dbadf8f1148ded11faa18e0546
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-03 19:01:34 -1000

    added logo to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,11 +2,26 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header>
-        {/* Logo */}
-      <div></div>
+    <header className="flex justify-between items-center px-3 border py-3">
+      {/* Logo */}
+      <div className="flex gap-3 items-center ">
+        <div>
+          <img src="./brush.png" alt="" />
+        </div>
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-bold text-[#36B864] ">
+            Pro<span className="text-black">Cleaning</span>
+          </h1>
+          <p className="text-[12px]">Cleaning Service Company</p>
+        </div>
+      </div>
+      {/* Hamburger */}
+      <nav className="flex flex-col gap-1.5">
+        <div className="px-4 py-0.5 rounded bg-[#36B864] "></div>
+        <div className="px-4 py-0.5 rounded bg-[#36B864] "></div>
+        <div className="px-4 py-0.5 rounded bg-[#36B864] "></div>
+      </nav>
       {/* Nav Info */}
-    <nav></nav>
     </header>
   );
 }
