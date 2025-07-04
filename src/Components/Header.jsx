918b967d2e88f2c020fa891281280d8c0be200eb
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-03 18:19:42 -1000

    initializing Business Cleaning Services

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+        {/* Logo */}
+      <div></div>
+      {/* Nav Info */}
+      <nav></nav>
+    </header>
+  );
+}
