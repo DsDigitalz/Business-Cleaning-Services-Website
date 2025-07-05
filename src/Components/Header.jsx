cf8d5c5655e46b4d4ca922a68141db2f4eb1ec32
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-04 19:37:08 -1000

    modified the nav logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center px-3 py-3">
       {/* Logo */}
-      <div className="flex gap-3 items-center ">
+      <div className="flex gap-2 items-center ">
         <div>
-          <img src="./brush.png" alt="" className="w-[20px]"/>
+          <img src="./brush.png" alt="" className="w-[20px]" />
         </div>
         <div className="flex flex-col">
-          <h1 className="text-xl font-bold text-[#36B864] ">
+          <h1 className="text-[19px] font-bold text-[#36B864] ">
             Pro<span className="text-black">Cleaning</span>
           </h1>
           <p className="text-[10px]">Cleaning Service Company</p>
