6154da5621bc0ed790af9e3e435a1cc11a7746de
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-04 22:27:31 -1000

    changed font to poppins

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="max-w-[1440px] mx-auto px-5 py-10 bg-[#FFFFF0]">
+      <div>
+        <img src="/a-img.png" alt="" />
+      </div>
+      <div className="flex flex-col items-center justify-center text-center mt-10 gap-2 md:items-start md:text-left ">
+        <h1 className="text-4xl font-bold lg:w-[70%] lg:text-[36px] xl:w-[80%]">
+          Welcome To Our Pro-cleaning Company!
+        </h1>
+        <p className="text-zinc-500 my-3 w-[70%]">
+          We provide comprehensive cleaning services tailored to your needs.
+          From residential cleaning services
+        </p>
+        <button className="cursor-pointer bg-[#2a8f4c] rounded text-white py-3 px-5 font-semibold active:bg-[#21723c]">
+          Get a quote
+        </button>
+      </div>
+    </section>
+  );
 }
