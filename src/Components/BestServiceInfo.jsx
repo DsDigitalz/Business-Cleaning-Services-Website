19dedef78712fc2f1cbaa706e9a20a7d7038ba5d
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-04 20:37:05 -1000

    added card and the info to QS component

--- src/Components/BestServiceInfo.jsx
+++ src/Components/BestServiceInfo.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function BestServiceInfo() {
   return (
-    <section className="bg-amber-50">
-      <div className="max-w-[1440px] mx-auto flex flex-col border-b border-zinc-300 mb-10 items-center pt-5 px-10 text-[#111D15] mt-10 bg-amber-50 md:flex-row md:items-center md:gap-10 md:px-10 md:py-10 lg:px-40">
-        <div className=" text-3xl font-bold w-[70%] text-center p-2 sm:w-[60%] md:flex md:items-center md:w-[70%] md:text-left md:mt-0 lg:text-[44px]">
+    <section className="bg-white">
+      <div className="max-w-[1440px] mx-auto flex flex-col border-b border-zinc-300 mb-10 items-center pt-5 px-10 text-[#111D15] mt-10 md:flex-row md:items-center md:gap-10 md:px-20 md:py-10 lg:px-40">
+        <div className=" text-3xl font-bold w-[90%] text-center  py-2 sm:w-[60%] md:flex md:items-center md:text-left md:mt-0 lg:text-[44px] bg-[#FAF9F6] md:p-5 md:w-[50%] lg:w-[40%]">
           We Always Provide The Best Service
         </div>
 
