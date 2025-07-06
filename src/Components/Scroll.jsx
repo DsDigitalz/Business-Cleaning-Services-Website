14ae3d02e7b904555197da42583232047398649d
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-06 01:55:25 -1000

    modified the Scroll component

--- src/Components/Scroll.jsx
+++ src/Components/Scroll.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { MdKeyboardDoubleArrowDown } from "react-icons/md";
+import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 export default function Scroll() {
   return (
     <section>
-        <div className='fixed bottom-20 right-10 bg-[#ffffff85] p-4 shadow shadow-zinc-200 rounded-full text-3xl'>
-      <MdKeyboardDoubleArrowDown />
+        <div className='fixed bottom-20 right-3  lg:right-5 bg-[#ffffff85] text-2xl p-2 lg:p-3 shadow shadow-zinc-200 rounded-full lg:text-3xl'>
+      <MdKeyboardDoubleArrowUp />
       </div>
     </section>
   )
