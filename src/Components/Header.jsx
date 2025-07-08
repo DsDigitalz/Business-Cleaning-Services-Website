c734d1fd6bdea82b1491923f0aac0230186e632c
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-07 16:10:46 -1000

    added overflow-hidden

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
   }
 
   return (
-    <header className=" flex flex-col lg:flex-row lg:justify-between px-5 lg:px-10 py-3">
+    <header className="relative flex flex-col lg:flex-row lg:justify-between px-5 lg:px-10 py-3 ">
       {/* Logo */}
       <div className="flex justify-between items-center ">
         <div className="flex gap-2">
@@ -32,7 +32,7 @@ export default function Header() {
         {/* Hamburger */}
         <button
           onClick={toggleMenu}
-          className="relative flex flex-col gap-1 lg:hidden"
+          className="relative flex flex-col gap-1 lg:hidden overflow-hidden"
         >
           <div
             className={`px-3.5 py-0.5 rounded bg-[#36B864] transition-all ${
@@ -52,33 +52,34 @@ export default function Header() {
         </button>
       </div>
       {/* Nav Info */}
-      <nav
-        className={`lg:flex lg:justify-between lg:items-center ${
-          isOpen ? "block" : "hidden"
-        } 
-        `}
-      >
-        <ul className="bg-white w-full absolute top-15 right-0 z-10 py-10 lg:py-0 flex  flex-col gap-3 lg:flex-row items-center lg:gap-10 font-medium  lg:visible lg:static ">
-          <li className="text-center  rounded bg-[#c0fad45b] w-full lg:w-fit p-3 lg:p-0 cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b  active:text-[#21723c] lg:bg-white ">
-            <a href="#home">Home</a>
-          </li>
-          <li className="text-center  bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-bg-zinc-50  hover:text-[#36B864] hover:pb-1 hover:border-b  active:text-[#21723c] p-3 lg:p-0 w-full lg:w-fit rounded lg:bg-white">
-            <a href="#services">Services</a>
-          </li>
-          <li className="text-center  bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b  active:text-[#21723c] p-3 w-full lg:p-0 lg:w-fit rounded lg:bg-white">
-            <a href="#about">About Us</a>
-          </li>
-          <li className="text-center  bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b  active:text-[#21723c] p-3 w-full lg:w-fit lg:p-0 rounded lg:bg-white">
-            <a href="#blog">Blog</a>
-          </li>
-          <li className="text-center  bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b  active:text-[#21723c] p-3 w-full lg:w-fit lg:p-0 rounded lg:bg-white">
-            <a href="#contact">Contact</a>
-          </li>
-          <button className="transition-all duration-300 cursor-pointer bg-[#2a8f4c] rounded text-white py-3 px-5 font-semibold active:bg-[#21723c] w-full lg:w-34  lg:block">
-            Get a quote
-          </button>
-        </ul>
-      </nav>
+        <nav className={`lg:flex lg:justify-between lg:items-center overflow-hidden`}>
+          <div
+            className={`lg:static lg:translate-x-0 transition-transform duration-500 ease-in-out ${
+              isOpen ? "translate-x-0" : "translate-x-full  "
+            } absolute top-12 right-0 w-full z-20 bg-white lg:block overflow-hidden`}
+          >
+            <ul className="overflow-hidden py-10 lg:py-0 flex flex-col gap-3 lg:flex-row items-center lg:gap-10 font-medium lg:visible lg:static over">
+              <li className="text-center rounded bg-[#c0fad45b] w-full lg:w-fit p-3 lg:p-0 cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b active:text-[#21723c] lg:bg-white">
+                <a href="#home">Home</a>
+              </li>
+              <li className="text-center bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b active:text-[#21723c] p-3 lg:p-0 w-full lg:w-fit rounded lg:bg-white">
+                <a href="#services">Services</a>
+              </li>
+              <li className="text-center bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b active:text-[#21723c] p-3 w-full lg:p-0 lg:w-fit rounded lg:bg-white">
+                <a href="#about">About Us</a>
+              </li>
+              <li className="text-center bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b active:text-[#21723c] p-3 w-full lg:w-fit lg:p-0 rounded lg:bg-white">
+                <a href="#blog">Blog</a>
+              </li>
+              <li className="text-center bg-[#c0fad45b] cursor-pointer hover:scale-103 transition-all duration-300 hover:text-[#36B864] hover:pb-1 hover:border-b active:text-[#21723c] p-3 w-full lg:w-fit lg:p-0 rounded lg:bg-white">
+                <a href="#contact">Contact</a>
+              </li>
+              <button className="transition-all duration-300 cursor-pointer bg-[#2a8f4c] rounded text-white py-3 px-5 font-semibold active:bg-[#21723c] w-full lg:w-34 lg:block">
+                Get a quote
+              </button>
+            </ul>
+          </div>
+        </nav>
     </header>
   );
 }
